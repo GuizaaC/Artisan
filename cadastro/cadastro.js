66f0fe1525ea0acd5efbf9bef5652de38304961d
Guilherme Izaac <42143794+GuizaaC@users.noreply.github.com>
2022-05-26 09:25:36 -0300

    "é" incluso

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,35 +1,35 @@
 //      ##########campos obrigatórios##########
 function fnValidar(){
     if (document.getElementById("Nome_Cliente").value==0){
-        alert("Campo nome obrigatório");
+        alert("Campo nome é obrigatório");
         document.getElementById("Nome_Cliente").focus();
      return false
     }
 
     if (document.getElementById("Endereco_Cliente").value==0){
-        alert("Campo endereço obrigatório");
+        alert("Campo endereço é obrigatório");
         document.getElementById("Endereco_Cliente").focus();
      return false
     }
     if (document.getElementById("Telefone_Cliente").value==0){
-        alert("Campo telefone obrigatório");
+        alert("Campo telefone é obrigatório");
         document.getElementById("Telefone_Cliente").focus();
      return false
     }
 
     if (document.getElementById("CPF_Cliente").value==0){
-        alert("Campo CPF obrigatório");
+        alert("Campo CPF é obrigatório");
         document.getElementById("CPF_Cliente").focus();
      return false
     }
 
     if (document.getElementById("Data_Cliente").value==0){
-        alert("Campo data de nascimento obrigatório");
+        alert("Campo data de nascimento é obrigatório");
         document.getElementById("Data_Cliente").focus();
      return false
     }
     if (document.getElementById("email_Cliente").value==0){
-        alert("Campo email obrigatório");
+        alert("Campo email é obrigatório");
         document.getElementById("email_Cliente").focus();
      return false
     }
