fbcaed8f45fc56e62369a9904db1ec500a02a4e9
Guilherme Izaac <42143794+GuizaaC@users.noreply.github.com>
2022-06-02 09:28:01 -0300

    teste5

--- cadastro/resultadoConsulta.php
+++ cadastro/resultadoConsulta.php
@@ -36,33 +36,17 @@ while ($rowp = mysqli_fetch_array($resp)) {
 
 //mysqli_select_db($connection,$database);
 
-echo "chegou aqui4";
-/*
-$connection = mysqli_connect($host,$user,$pass,$dbname) or die (mysql_errno().": ".mysql_error()."<BR>"); 
 
-mysqli_select_db($connection,$dbname);
+
+$connection = mysqli_connect($host,$user,$pass,$database) or die (mysql_errno().": ".mysql_error()."<BR>"); 
+
+mysqli_select_db($connection,$database);
 // where nome='".$nome."' and cpf='".$cpf."' and Telefone='".$Telefone."' and eMail='".$eMail."'"
 
 echo $query;
 
 
 
-$resp= mysqli_query ($connection,$query) or die ('Erro ao salvar');
-
-
-
-			//	$resp = mysqlexecuta($connection,$sqlp);
-
-						while ($rowp = mysqli_fetch_array($resp)) {							
-
-							
-//{"Id_Cliente":"1"}
-											//buscando do campo do banco de dados DT_DE_ALTERACAO.
-
-											
-
-												echo "<BR/><a href='cadastro.php?Id_Cliente=".$rowp["Id_Cliente"]."'>ALTERAR ID".$rowp["Id_Cliente"]."</a> ";
-						}
-						*/
+echo "chegou aqui5";
 ?>
 
