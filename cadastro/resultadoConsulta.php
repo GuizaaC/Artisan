e5f274cacd02fe34f0284865112bbfd6db77a008
Guilherme Izaac <42143794+GuizaaC@users.noreply.github.com>
2022-06-02 09:23:11 -0300

    teste3

--- cadastro/resultadoConsulta.php
+++ cadastro/resultadoConsulta.php
@@ -1,19 +1,35 @@
 mensagem.
 <?php
+
+
+
 	$servername = "localhost";
-	$username = "u553234134_Gizaac";
-	$password = "Gizaac12343124";
-	$database = "u553234134_Artisan";
+$database = "u553234134_Artisan";
+$username = "u553234134_Gizaac";
+$password = "Gizaac12343124";
+// Create connection
+$connection = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$query = " select Id_Cliente from Cliente ";
+
+
+
+
+echo $query;
+mysqli_query($connection,$query) or die ('Erro ao consultar..');
+echo "Salvo com sucesso";
+	
+
+echo "chegou aqui3";
+/*
 $connection = mysqli_connect($host,$user,$pass,$dbname) or die (mysql_errno().": ".mysql_error()."<BR>"); 
 
 mysqli_select_db($connection,$dbname);
-
-
-	
-
-$query = " select Id_Cliente from Cliente ";
-echo "chegou aqui3";
-/*
 // where nome='".$nome."' and cpf='".$cpf."' and Telefone='".$Telefone."' and eMail='".$eMail."'"
 
 echo $query;
