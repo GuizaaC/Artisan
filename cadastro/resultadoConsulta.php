81c8f4366515dece9c13a0904a678f3feb7dfbd2
Guilherme Izaac <42143794+GuizaaC@users.noreply.github.com>
2022-06-03 23:14:03 -0300

    formatação

--- cadastro/resultadoConsulta.php
+++ cadastro/resultadoConsulta.php
@@ -1,33 +1,27 @@
-mensagem.
 <?php
+//DB Info
 	$servername = "localhost";
 	$database = "u553234134_Artisan";
 	$username = "u553234134_Gizaac";
 	$password = "Gizaac12343124";
+
 // Create connection
 	$connection = mysqli_connect($servername, $username, $password, $database);
+
 // Check connection
 	if (!$connection) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
 
-
+//Selecting DB fields for selection
 	$query = " select Id_Cliente from Cliente ";
-
+//
 	echo $query;
 	$resp= mysqli_query($connection,$query) or die ('Erro ao consultar..');
 
 	while ($rowp = mysqli_fetch_array($resp)) {							
 
-							
-	//{"Id_Cliente":"1"}
-												//buscando do campo do banco de dados DT_DE_ALTERACAO.
-	
-												
-	
 	echo "<BR/><a href='cadastro.php?Id_Cliente=".$rowp["Id_Cliente"]."'>ALTERAR ID".$rowp["Id_Cliente"]."</a> ";
 	};
-
-//mysqli_select_db($connection,$database);
 ?>
 
--- cadastro/salvar.php
+++ cadastro/salvar.php
@@ -1,17 +1,17 @@
 <?php
 //dados do banco
-$servername = "localhost";
-$database = "u553234134_Artisan";
-$username = "u553234134_Gizaac";
-$password = "Gizaac12343124";
+    $servername = "localhost";
+    $database = "u553234134_Artisan";
+    $username = "u553234134_Gizaac";
+    $password = "Gizaac12343124";
 
 // Create connection
-$connection = mysqli_connect($servername, $username, $password, $database);
+    $connection = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 //Comandos de transferencia(PHP - SQL)
     $query = " INSERT INTO Cliente
         ( Email_Cliente, Nome_Cliente, Endereco_Cliente, Telefone_Cliente, Cpf_Cliente, Data_Nascimento_Cliente, Senha_Cliente)
@@ -19,12 +19,12 @@ if (!$connection) {
         '".$_POST["Cpf_Cliente"]."', '".$_POST["Data_Nascimento_Cliente"]."','".$_POST["Senha_Cliente"]."');";
 
 //Confirmação do salvamento
-echo $query;
-mysqli_query($connection,$query) or die ('Erro ao salvar..');
-echo "Salvo com sucesso";
-?>
+    echo $query;
+    mysqli_query($connection,$query) or die ('Erro ao salvar..');
+    echo "Salvo com sucesso";
+    ?>
 
 
-<script>
-    window.location='../perfil_usuario/perfil.html';
-</script>
+    <script>
+        window.location='../perfil_usuario/perfil.html';
+    </script>
