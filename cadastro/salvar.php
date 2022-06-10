aa7e5f522d8ee9829e35efd11d0b0fd00b9271fd
Guilherme Izaac <42143794+GuizaaC@users.noreply.github.com>
2022-06-09 22:30:19 -0300

    media

--- cadastro/salvar.php
+++ cadastro/salvar.php
@@ -5,10 +5,10 @@
     $username = "u553234134_Gizaac";
     $password = "Gizaac12343124";
 
-// Create connection
+// Criação de conexão
     $connection = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
+// Checagem de conexão
     if (!$connection) {
         die("Connection failed: " . mysqli_connect_error());
     }
--- delete_cliente/delete.php
+++ delete_cliente/delete.php
@@ -1,17 +1,18 @@
 <?php
+//dados do banco
     $servername = "localhost";
     $database = "u553234134_Artisan";
     $username = "u553234134_Gizaac";
     $password = "Gizaac12343124";
 
-// Create connection
+// Criação de conexão
 $connection = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
+// Checagem de conexão
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
+// Deletar do banco
 $query = " DELETE FROM Cliente WHERE  Id_Cliente = '".$_GET["Id_Cliente"]."' ";
 
 echo $query;
