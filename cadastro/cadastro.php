36cbcfa1b6647d8f854024f1bcb12002ca37a1ea
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-04-23 22:36:32 -0300

    alterações para o novo banco de dados

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -30,25 +30,25 @@ if(isset($_POST['Email_Cliente'])){
     <form id="formSalvar" action='salvar.php' method="POST" enctype = "multipart/form-data" onsubmit>
             <!-- Campo nome -->
             <label class="titulo">Nome</label> <br>  
-                <input class="entrada" name='Nome_Cliente' id="Nome_Cliente" placeholder="Nome" required> <br>
+                <input class="entrada" name='nome_usuario' id="nome_usuario" placeholder="Nome" required> <br>
             <!-- Campo Endereço -->
             <label class="titulo">Endereço</label> <br>
-                <input class="entrada" name='Endereco_Cliente' id="Endereco_Cliente" placeholder="Endereço" required>  <br>
+                <input class="entrada" name='endereco_usuario' id="endereco_usuario" placeholder="Endereço" required>  <br>
             <!-- Campo Telefone -->
             <label class="titulo">Telefone</label> <br>
-                <input class="entrada" type="tel" maxlength="13"name='Telefone_Cliente' id="Telefone_Cliente" placeholder="(00)90000-0000"  onkeydown = "mascara_telefone()" required> <br>
+                <input class="entrada" type="tel" maxlength="13"name='telefone_usuario' id="telefone_usuario" placeholder="(00)90000-0000"  onkeydown = "mascara_telefone()" required> <br>
             <!-- Campo CPF -->
             <label class="titulo">CPF</label> <br>
-                <input class="entrada"  maxlength="14"name='Cpf_Cliente' id="CPF_Cliente" placeholder="000.000.000-00"  onkeydown = "mascara_cpf()" required > <br>
+                <input class="entrada"  maxlength="14"name='cpf_usuario' id="cpf_usuario" placeholder="000.000.000-00"  onkeydown = "mascara_cpf()" required > <br>
             <!-- Campo Data de nascimento -->
             <label class="titulo">Data de Nascimento</label> <br>
-                <input class="entrada" min="1900-01-01" max="2100-12-31" type="date" name='Data_Nascimento_Cliente' id="Data_Cliente" placeholder="DD/MM/AAAA" required> <br>
+                <input class="entrada" min="1900-01-01" max="2100-12-31" type="date" name='data_nascimento_usuario' id="data_usuario" placeholder="DD/MM/AAAA" required> <br>
             <!-- Campo Email -->
             <label class="titulo">Email</label> <br>
-                <input class="entrada" type="email" max="70" name='Email_Cliente' id="email_Cliente" placeholder="Email" required> <br>
+                <input class="entrada" type="email" max="70" name='email_usuario' id="email_usuario" placeholder="Email" required> <br>
             <!-- Campo senha -->
             <label class="titulo">Senha</label>
-                <input class="entrada" type="password" min="5" max="10" name="Senha_Cliente" id="senha_cliente" placeholder="Senha" required><br>
+                <input class="entrada" type="password" min="5" max="10" name="senha_usuario" id="senha_usuario" placeholder="Senha" required><br>
             <label class="titulo">Foto do perfil</label>
                         <input class = "imagem" type = "file" name = " Img_Cliente"><br>
 
