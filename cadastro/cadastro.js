1314bc347c7a34e7517f160e19ee2e50f1a82a20
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-04-24 07:59:56 -0300

    alterações no JS

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,41 +1,41 @@
 //      ##########campos obrigatórios##########
 function fnValidar(){
-    if (document.getElementById("Nome_Cliente").value==0){
+    if (document.getElementById("nome_usuario").value==0){
         alert("Campo nome é obrigatório");
-        document.getElementById("Nome_Cliente").focus();
+        document.getElementById("nome_usuario").focus();
      return false
     }
 
-    if (document.getElementById("Endereco_Cliente").value==0){
+    if (document.getElementById("endereco_usuario").value==0){
         alert("Campo endereço é obrigatório");
-        document.getElementById("Endereco_Cliente").focus();
+        document.getElementById("endereco_usuario").focus();
      return false
     }
-    if (document.getElementById("Telefone_Cliente").value==0){
+    if (document.getElementById("telefone_usuario").value==0){
         alert("Campo telefone é obrigatório");
-        document.getElementById("Telefone_Cliente").focus();
+        document.getElementById("telefone_usuario").focus();
      return false
     }
 
-    if (document.getElementById("CPF_Cliente").value==0){
+    if (document.getElementById("cpf_usuario").value==0){
         alert("Campo CPF é obrigatório");
-        document.getElementById("CPF_Cliente").focus();
+        document.getElementById("cpf_usuario").focus();
      return false
     }
 
-    if (document.getElementById("Data_Cliente").value==0){
+    if (document.getElementById("data_usuario").value==0){
         alert("Campo data de nascimento é obrigatório");
-        document.getElementById("Data_Cliente").focus();
+        document.getElementById("data_usuario").focus();
      return false
     }
-    if (document.getElementById("email_Cliente").value==0){
+    if (document.getElementById("email_usuario").value==0){
         alert("Campo email é obrigatório");
-        document.getElementById("email_Cliente").focus();
+        document.getElementById("email_usuario").focus();
      return false
     }
-    if (document.getElementById("senha_cliente").value==0){
+    if (document.getElementById("senha_usuario").value==0){
         alert("Campo senha é obrigatório");
-        document.getElementById("senha_cliente").focus();
+        document.getElementById("senha_usuario").focus();
      return false
     }
     
