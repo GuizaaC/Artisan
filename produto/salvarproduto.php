c6243d1f93cadf4024eb03d8781185815923cefa
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-04 23:32:05 -0300

    teste produto 2

--- produto/salvarproduto.php
+++ produto/salvarproduto.php
@@ -8,51 +8,26 @@
 // Criação de conexão
     $connection = mysqli_connect($servername, $username, $password, $database);
 
+
+
 // Checagem de conexão
     if (!$connection) {
         die("Connection failed: " . mysqli_connect_error());
     }
 //Comandos de transferencia(PHP - SQL)
-    $query = " INSERT INTO Produto
-        ( Nome_Produto, Preco_Produto, Descricao_Produto)
-    VALUES('".$_POST["nome_Produto"]."', '".$_POST["preco_Produto"]."', '".$_POST["descricao_Produto"]."');";
-    /*  namespace Verot\Upload;
-    $foo = new Upload($_FILES['Img_Cliente']); 
-        if ($foo->uploaded) {
-        // save uploaded image with no changes
-        $foo->process('/imagem_cliente');
-        if ($foo->processed) {
-            echo 'Imagem original copiada';
-        } else {
-            echo 'error : ' . $foo->error;
-        }
-        // save uploaded image with a new name
-        $foo->file_new_name_body = 'foo';
-        $foo->process('/imagens');
-        if ($foo->processed) {
-            echo 'image renamed "foo" copied';
-        } else {
-            echo 'error : ' . $foo->error;
-        }   
-        // save uploaded image with a new name
-
-        $foo->file_new_name_body = 'image_resized';
-        $foo->image_resize = true;
-        $foo->image_convert = 'jpg';
-        $foo->image_x = 56;
-        $foo->image_y = 56;
-        $foo->image_ratio_y = true;
-        $foo->process('/imagem_cliente');
-        if ($foo->processed) {
-            echo 'image renamed, resized x=56, resized y=56
-                and converted to jpg';
-            $foo->clean();
-        } else {
-            echo 'error : ' . $foo->error;
-        } 
-    }  */
+    $query = " INSERT INTO produto
+        (nome_produto, preco_produto, descricao_produto, quantidade_produto, img_produto)
+    VALUES('".$_POST["nome_produto"]."', '".$_POST["preco_produto"]."','".$_POST["descricao_produto"]."', 
+        '".$_POST["quantidade_produto"]."', '".$_POST["img_produto"]."');";
 
 //Confirmação do salvamento
     echo $query;
     mysqli_query($connection,$query) or die ('Erro ao salvar..');
     echo "Salvo com sucesso";
+
+        ?>
+
+
+    <script>
+        window.location='../home/produtos.php';
+    </script>
