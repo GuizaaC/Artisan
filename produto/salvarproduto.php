50c3038a3981597e71a74fdbdf39c869360889fb
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-04 23:35:44 -0300

    teste produto 3

--- produto/salvarproduto.php
+++ produto/salvarproduto.php
@@ -1,33 +1,56 @@
 <?php
-//dados do banco
-    $servername = "localhost";
-    $database = "u553234134_Artisan";
-    $username = "u553234134_Gizaac";
-    $password = "Gizaac12343124";
+// Dados do banco
+$servername = "localhost";
+$database = "u553234134_Artisan";
+$username = "u553234134_Gizaac";
+$password = "Gizaac12343124";
 
 // Criação de conexão
-    $connection = mysqli_connect($servername, $username, $password, $database);
-
-
+$connection = mysqli_connect($servername, $username, $password, $database);
 
 // Checagem de conexão
-    if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Função para validar e sanitizar os dados recebidos via POST
+function sanitize($data) {
+    // Implemente a sanitização apropriada para os dados
+    // por exemplo, você pode usar mysqli_real_escape_string() ou outras funções de filtragem
+    // certifique-se de aplicar a função sanitize() a todos os campos necessários antes de usá-los na consulta SQL
+    return $data;
+}
+
 //Comandos de transferencia(PHP - SQL)
-    $query = " INSERT INTO produto
-        (nome_produto, preco_produto, descricao_produto, quantidade_produto, img_produto)
-    VALUES('".$_POST["nome_produto"]."', '".$_POST["preco_produto"]."','".$_POST["descricao_produto"]."', 
-        '".$_POST["quantidade_produto"]."', '".$_POST["img_produto"]."');";
+$query = "INSERT INTO produto (nome_produto, preco_produto, descricao_produto, quantidade_produto, img_produto) 
+          VALUES (?, ?, ?, ?, ?)";
 
-//Confirmação do salvamento
-    echo $query;
-    mysqli_query($connection,$query) or die ('Erro ao salvar..');
-    echo "Salvo com sucesso";
+$stmt = mysqli_prepare($connection, $query);
+if ($stmt) {
+    // Sanitiza os dados antes de vinculá-los aos parâmetros do statement
+    $nome_produto = sanitize($_POST["nome_produto"]);
+    $preco_produto = sanitize($_POST["preco_produto"]);
+    $descricao_produto = sanitize($_POST["descricao_produto"]);
+    $quantidade_produto = sanitize($_POST["quantidade_produto"]);
+    $img_produto = sanitize($_POST["img_produto"]);
 
-        ?>
+    // Vincula os parâmetros aos placeholders na consulta
+    mysqli_stmt_bind_param($stmt, "ssssi", $nome_produto, $preco_produto, $descricao_produto, $quantidade_produto, $img_produto);
 
+    // Executa o statement
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Salvo com sucesso";
+        mysqli_stmt_close($stmt);
+    } else {
+        die('Erro ao salvar..');
+    }
+} else {
+    die('Erro na preparação da consulta.');
+}
 
-    <script>
-        window.location='../home/produtos.php';
-    </script>
+mysqli_close($connection);
+
+// Redireciona o usuário para "../home/produtos.php"
+header("Location: ../home/produtos.php");
+exit();
+?>
