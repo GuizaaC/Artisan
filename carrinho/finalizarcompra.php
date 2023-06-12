650a6e63ae5db6bd5d35bed944a0e399c5d7e644
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-12 07:59:53 -0300

    finalizar compra

--- carrinho/finalizarcompra.php
+++ carrinho/finalizarcompra.php
@@ -1,38 +1,19 @@
 <?php
-//dados do banco
-    $servername = "localhost";
-    $database = "u553234134_Artisan";
-    $username = "u553234134_Gizaac";
-    $password = "Gizaac12343124";
+session_start(); // Inicia a sessão (se já não estiver iniciada)
 
-// Criação de conexão
-    $connection = mysqli_connect($servername, $username, $password, $database);
-
-// Checagem de conexão
-    if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-//Comandos de transferencia(PHP - SQL)
-    $query = " INSERT INTO usuario
-        (email_usuario , nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, senha_usuario)
-    VALUES('".$_POST["email_usuario"]."', '".$_POST["nome_usuario"]."', '".$_POST["endereco_usuario"]."','".$_POST["telefone_usuario"]."', 
-        '".$_POST["cpf_usuario"]."', '".$_POST["data_nascimento_usuario"]."',('".$_POST["senhaCriptografada"]."'));";
-//Confirmação do salvamento
-    echo $query;
-    mysqli_query($connection,$query) or die ('Erro ao salvar..');
-    echo "Salvo com sucesso";
-
-        $query = " select id_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, email_usuario, senha_usuario from 
-        usuario Where email_usuario = '".$_POST["email_usuario"]."' and senha_usuario = '".$_POST["senha_usuario"]."'" ;
-        $resp= mysqli_query($connection,$query) or die ('Erro ao consultar..');
-        $_SESSION["logado"]=  "nao";
-        while ($rowp = mysqli_fetch_array($resp)) {	        
-            $_SESSION["id_usuario"]=  $rowp["id_usuario"];   
-        };
+// Verifica se o usuário está logado
+if ($_SESSION["logado"] !== "sim") {
+    // Usuário não está logado, redirecione para a página de login ou exiba uma mensagem de erro.
+    header("Location: ../cadastro/cadastro.php"); // Redireciona para a página de login
+    exit; // Encerra a execução do script atual
+}else{
+    header("Location: ../home/home.php"); // Redireciona para a página de login
+    exit; // Encerra a execução do script atual
+}
 
         ?>
 
 
     <script>
-        window.location='../listar_usuarios/listarUsuarios.php';
+        alert("Obrigado por comprar conosco!")
     </script>
