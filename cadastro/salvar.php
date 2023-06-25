1fc605c4e0a1324533632b8b36aef59a5f25e299
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-24 22:27:12 -0300

    teste imagens 4

--- cadastro/salvar.php
+++ cadastro/salvar.php
@@ -17,16 +17,9 @@ if (!$connection) {
 
 // Verifica se um arquivo de imagem foi enviado
 if (isset($_FILES['img_usuario']) && $_FILES['img_usuario']['error'] === UPLOAD_ERR_OK) {
-    $targetDir = '...assets';  // Substitua pelo caminho real do diretório
+    $targetDir = 'assets';  // Substitua pelo caminho real do diretório
     $targetFile = $targetDir . basename($_FILES['img_usuario']['name']);
     $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-
-    // Exibe informações para depuração
-    echo "Nome do arquivo: " . $_FILES['img_usuario']['name'] . "<br>";
-    echo "Tipo do arquivo: " . $_FILES['img_usuario']['type'] . "<br>";
-    echo "Tamanho do arquivo: " . $_FILES['img_usuario']['size'] . "<br>";
-    echo "Caminho temporário: " . $_FILES['img_usuario']['tmp_name'] . "<br>";
-    echo "Erro no upload: " . $_FILES['img_usuario']['error'] . "<br>";
 
     // Verifica se o arquivo é uma imagem válida
     $allowedExtensions = array('jpg', 'jpeg', 'png');
@@ -34,27 +27,25 @@ if (isset($_FILES['img_usuario']) && $_FILES['img_usuario']['error'] === UPLOAD_
         // Move o arquivo para o diretório de destino
         if (move_uploaded_file($_FILES['img_usuario']['tmp_name'], $targetFile)) {
             // Caminho da imagem para salvar no banco de dados
-            $caminhoImagem = $targetFile;
+            $imgUsuario = $targetFile;
 
             // Aplicar criptografia MD5 à senha
             $senhaCriptografada = md5($_POST["senha_usuario"]);
 
             // Comandos de transferência (PHP - SQL)
-            $query = "INSERT INTO usuario (email_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, senha_usuario, caminho_imagem)
+            $query = "INSERT INTO usuario (email_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, senha_usuario, img_usuario)
                       VALUES ('".$_POST["email_usuario"]."', '".$_POST["nome_usuario"]."', '".$_POST["endereco_usuario"]."', '".$_POST["telefone_usuario"]."', 
-                      '".$_POST["cpf_usuario"]."', '".$_POST["data_nascimento_usuario"]."', '".$senhaCriptografada."', '".$caminhoImagem."');";
+                      '".$_POST["cpf_usuario"]."', '".$_POST["data_nascimento_usuario"]."', '".$senhaCriptografada."', '".$imgUsuario."');";
 
             // Confirmação do salvamento
-            echo "Query: " . $query . "<br>";
-
-            mysqli_query($connection, $query) or die ('Erro ao salvar..');
+            mysqli_query($connection, $query) or die ('Erro ao salvar: ' . mysqli_error($connection));
             echo "Salvo com sucesso";
 
             $query = "SELECT id_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, email_usuario, senha_usuario 
                       FROM usuario 
                       WHERE email_usuario = '".$_POST["email_usuario"]."' AND senha_usuario = '".$senhaCriptografada."';";
 
-            $resp = mysqli_query($connection, $query) or die ('Erro ao consultar..');
+            $resp = mysqli_query($connection, $query) or die ('Erro ao consultar: ' . mysqli_error($connection));
             $_SESSION["logado"] = "nao";
             while ($rowp = mysqli_fetch_array($resp)) {
                 $_SESSION["id_usuario"] = $rowp["id_usuario"];
@@ -63,20 +54,18 @@ if (isset($_FILES['img_usuario']) && $_FILES['img_usuario']['error'] === UPLOAD_
             echo '<script>window.location = "../home/home.php";</script>';
 
         } else {
-            // Exibe mensagem de erro de upload
             echo "Erro ao fazer upload do arquivo.";
         }
     } else {
-        // Exibe mensagem de erro de tipo de arquivo
         echo "Apenas arquivos JPG, JPEG e PNG são permitidos.";
     }
 } else {
-    // Exibe mensagem quando nenhum arquivo de imagem foi enviado
     echo "Nenhum arquivo de imagem foi enviado.";
 }
 
 mysqli_close($connection);
 ?>
+
 
 
 
