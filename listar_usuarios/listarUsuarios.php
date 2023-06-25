821a9c8eb9ef6fb02288706cb57f65b3fd968572
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-25 09:17:03 -0300

    teste 2

--- listar_usuarios/listarUsuarios.php
+++ listar_usuarios/listarUsuarios.php
@@ -13,7 +13,7 @@
 <body>
 <!-- cabeçalho -->
 <header class="topo">
-    <a href="home/home.html">
+    <a href=".../home.html">
         <img src="../assets/Logo.png" alt="Logo" class="logo">
     </a>
 </header>
