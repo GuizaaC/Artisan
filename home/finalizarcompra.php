fd225084fd19532f206ebf4849b3036f80c88e7d
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-25 20:48:45 -0300

    teste finalizar compra

--- home/finalizarcompra.php
+++ home/finalizarcompra.php
@@ -2,9 +2,9 @@
 session_start(); // Inicia a sessão (se já não estiver iniciada)
 
 // Verifica se o usuário está logado
-if ($_SESSION["logado"] !== "sim") {
+if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== "sim") {
     // Usuário não está logado, redirecione para a página de login ou exiba uma mensagem de erro.
-    header("Location: /cadastro/cadastro.php"); // Redireciona para a página de login
+    header("Location: /login/login.php"); // Redireciona para a página de login
     exit; // Encerra a execução do script atual
 }
 
@@ -24,23 +24,18 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// Obtenção das informações do produto
+$id_produto = $_POST["id_produto"];
+$nome_produto = $_POST["nome_produto"];
+$preco_produto = $_POST["preco_produto"];
+
+// Resto do código para processar a finalização da compra
+
 // Comandos de transferência (PHP - SQL)
-$query = "INSERT INTO usuario (email_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, senha_usuario)
-    VALUES ('".$_POST["email_usuario"]."', '".$_POST["nome_usuario"]."', '".$_POST["endereco_usuario"]."','".$_POST["telefone_usuario"]."', 
-    '".$_POST["cpf_usuario"]."', '".$_POST["data_nascimento_usuario"]."',('".$_POST["senhaCriptografada"]."'));";
+$query = "INSERT INTO carrinho (id_usuario, id_produto, nome_produto, preco_produto) VALUES ('".$_SESSION["id_usuario"]."', '".$id_produto."', '".$nome_produto."', '".$preco_produto."');";
 
 // Confirmação do salvamento
-echo $query;
 mysqli_query($connection, $query) or die('Erro ao salvar..');
-echo "Salvo com sucesso";
-
-$query = "SELECT id_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, email_usuario, senha_usuario 
-    FROM usuario WHERE email_usuario = '".$_POST["email_usuario"]."' AND senha_usuario = '".$_POST["senha_usuario"]."'";
-$resp = mysqli_query($connection, $query) or die('Erro ao consultar..');
-$_SESSION["logado"] = "nao";
-while ($rowp = mysqli_fetch_array($resp)) {
-    $_SESSION["id_usuario"] = $rowp["id_usuario"];
-}
 
 mysqli_close($connection);
 
@@ -52,8 +47,6 @@ header('Location: /home/home.php');
 exit;
 ?>
 
-
-
-    <script>
-        alert("Obrigado por comprar conosco!")
-    </script>
+<script>
+    alert("Obrigado por comprar conosco!");
+</script>
