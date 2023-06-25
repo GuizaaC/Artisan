11e1899a0d6b1b4c06a4fa204b4d524bf36195c4
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-24 21:58:52 -0300

    salvar com echo de checagem

--- cadastro/salvar.php
+++ cadastro/salvar.php
@@ -1,5 +1,7 @@
 <?php
-//dados do banco
+session_start();
+
+// Dados do banco
 $servername = "localhost";
 $database = "u553234134_Artisan";
 $username = "u553234134_Gizaac";
@@ -13,32 +15,68 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Aplicar criptografia MD5 à senha
-$senhaCriptografada = md5($_POST["senha_usuario"]);
+// Verifica se um arquivo de imagem foi enviado
+if (isset($_FILES['img_usuario']) && $_FILES['img_usuario']['error'] === UPLOAD_ERR_OK) {
+    $targetDir = '...imagens';  // Substitua pelo caminho real do diretório
+    $targetFile = $targetDir . basename($_FILES['img_usuario']['name']);
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-//Comandos de transferencia(PHP - SQL)
-$query = "INSERT INTO usuario (email_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, senha_usuario)
-          VALUES ('".$_POST["email_usuario"]."', '".$_POST["nome_usuario"]."', '".$_POST["endereco_usuario"]."', '".$_POST["telefone_usuario"]."', 
-          '".$_POST["cpf_usuario"]."', '".$_POST["data_nascimento_usuario"]."', '".$senhaCriptografada."');";
+    // Exibe informações para depuração
+    echo "Nome do arquivo: " . $_FILES['img_usuario']['name'] . "<br>";
+    echo "Tipo do arquivo: " . $_FILES['img_usuario']['type'] . "<br>";
+    echo "Tamanho do arquivo: " . $_FILES['img_usuario']['size'] . "<br>";
+    echo "Caminho temporário: " . $_FILES['img_usuario']['tmp_name'] . "<br>";
+    echo "Erro no upload: " . $_FILES['img_usuario']['error'] . "<br>";
 
-//Confirmação do salvamento
-echo $query;
-mysqli_query($connection, $query) or die ('Erro ao salvar..');
-echo "Salvo com sucesso";
+    // Verifica se o arquivo é uma imagem válida
+    $allowedExtensions = array('jpg', 'jpeg', 'png');
+    if (in_array($imageFileType, $allowedExtensions)) {
+        // Move o arquivo para o diretório de destino
+        if (move_uploaded_file($_FILES['img_usuario']['tmp_name'], $targetFile)) {
+            // Caminho da imagem para salvar no banco de dados
+            $caminhoImagem = $targetFile;
 
-$query = "SELECT id_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, email_usuario, senha_usuario 
-          FROM usuario 
-          WHERE email_usuario = '".$_POST["email_usuario"]."' AND senha_usuario = '".$senhaCriptografada."';";
+            // Aplicar criptografia MD5 à senha
+            $senhaCriptografada = md5($_POST["senha_usuario"]);
 
-$resp = mysqli_query($connection, $query) or die ('Erro ao consultar..');
-$_SESSION["logado"] = "nao";
-while ($rowp = mysqli_fetch_array($resp)) {
-    $_SESSION["id_usuario"] = $rowp["id_usuario"];
-};
+            // Comandos de transferência (PHP - SQL)
+            $query = "INSERT INTO usuario (email_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, senha_usuario, caminho_imagem)
+                      VALUES ('".$_POST["email_usuario"]."', '".$_POST["nome_usuario"]."', '".$_POST["endereco_usuario"]."', '".$_POST["telefone_usuario"]."', 
+                      '".$_POST["cpf_usuario"]."', '".$_POST["data_nascimento_usuario"]."', '".$senhaCriptografada."', '".$caminhoImagem."');";
+
+            // Confirmação do salvamento
+            echo "Query: " . $query . "<br>";
+
+            mysqli_query($connection, $query) or die ('Erro ao salvar..');
+            echo "Salvo com sucesso";
+
+            $query = "SELECT id_usuario, nome_usuario, endereco_usuario, telefone_usuario, cpf_usuario, data_nascimento_usuario, email_usuario, senha_usuario 
+                      FROM usuario 
+                      WHERE email_usuario = '".$_POST["email_usuario"]."' AND senha_usuario = '".$senhaCriptografada."';";
+
+            $resp = mysqli_query($connection, $query) or die ('Erro ao consultar..');
+            $_SESSION["logado"] = "nao";
+            while ($rowp = mysqli_fetch_array($resp)) {
+                $_SESSION["id_usuario"] = $rowp["id_usuario"];
+            }
+
+            echo '<script>window.location = "../home/home.php";</script>';
+
+        } else {
+            // Exibe mensagem de erro de upload
+            echo "Erro ao fazer upload do arquivo.";
+        }
+    } else {
+        // Exibe mensagem de erro de tipo de arquivo
+        echo "Apenas arquivos JPG, JPEG e PNG são permitidos.";
+    }
+} else {
+    // Exibe mensagem quando nenhum arquivo de imagem foi enviado
+    echo "Nenhum arquivo de imagem foi enviado.";
+}
+
+mysqli_close($connection);
 ?>
 
-<script>
-    window.location = '../home/home.php';
-</script>x
 
 
