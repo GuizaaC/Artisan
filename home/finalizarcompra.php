3799f1913ea3d5df3e827c8f5765df1322f23b0b
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-25 21:34:26 -0300

    echo finalizar

--- home/finalizarcompra.php
+++ home/finalizarcompra.php
@@ -2,9 +2,9 @@
 session_start(); // Inicia a sessão (se já não estiver iniciada)
 
 // Verifica se o usuário está logado
-if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== "sim") {
+if ($_SESSION["logado"] !== "sim") {
     // Usuário não está logado, redirecione para a página de login ou exiba uma mensagem de erro.
-    header("Location: /login/login.php"); // Redireciona para a página de login
+    header("Location: /cadastro/cadastro.php"); // Redireciona para a página de login
     exit; // Encerra a execução do script atual
 }
 
@@ -24,18 +24,17 @@ if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Obtenção das informações do produto
-$id_produto = $_POST["id_produto"];
-$nome_produto = $_POST["nome_produto"];
-$preco_produto = $_POST["preco_produto"];
-
-// Resto do código para processar a finalização da compra
-
 // Comandos de transferência (PHP - SQL)
 $query = "INSERT INTO carrinho (id_usuario, id_produto, nome_produto, preco_produto) VALUES ('".$_SESSION["id_usuario"]."', '".$id_produto."', '".$nome_produto."', '".$preco_produto."');";
 
-// Confirmação do salvamento
-mysqli_query($connection, $query) or die('Erro ao salvar..');
+// Tentativa de salvar os dados no banco
+if (mysqli_query($connection, $query)) {
+    // Os dados foram salvos com sucesso
+    echo "Dados salvos com sucesso!";
+} else {
+    // Houve um erro ao salvar os dados
+    echo "Erro ao salvar os dados: " . mysqli_error($connection);
+}
 
 mysqli_close($connection);
 
@@ -48,5 +47,6 @@ exit;
 ?>
 
 <script>
-    alert("Obrigado por comprar conosco!");
+    alert("Obrigado por comprar conosco!")
 </script>
+
