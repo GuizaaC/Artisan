1991e5aa4762c6fba99d7cb9f097bc9befedf2ef
GuilhermeHCN <104985803+GuilhermeHCN@users.noreply.github.com>
2023-06-25 23:11:56 -0300

    teste finalização da venda

--- home/finalizarcompra.php
+++ home/finalizarcompra.php
@@ -30,7 +30,19 @@ $query = "INSERT INTO venda (data_venda, finalizada_venda) VALUES (NOW(), 1);";
 // Tentativa de salvar os dados no banco
 if (mysqli_query($connection, $query)) {
     // Os dados foram salvos com sucesso
-    echo "Dados salvos com sucesso!";
+
+    // Obter o ID do produto vendido (supondo que você tenha essa informação disponível)
+    $produtoId = $_POST['produto_id'];
+
+    // Atualizar o estoque do produto
+    $updateQuery = "UPDATE produto SET quantidade_produto = quantidade_produto - 1 WHERE id_produto = $produtoId";
+
+    // Tentativa de atualizar o estoque
+    if (mysqli_query($connection, $updateQuery)) {
+        echo "Dados salvos e estoque atualizado com sucesso!";
+    } else {
+        echo "Erro ao atualizar o estoque: " . mysqli_error($connection);
+    }
 } else {
     // Houve um erro ao salvar os dados
     echo "Erro ao salvar os dados: " . mysqli_error($connection);
@@ -46,6 +58,7 @@ echo "Usuário logado e compra finalizada.";
 header('Location: /home/home.php');
 exit;
 ?>
+
 
 
 
